e780bf8f62bffdaa74a2552bcde5a8bb6d1139a3
Ahmed Magdy <madimagdy@gmail.com>
2023-07-22 21:58:03 +0900

    add comments to the helper frontend scripts

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,19 +1,21 @@
+// grab the HTML elements using DOM Manipulation
 const addCommentForm = document.getElementById('add-comment-form');
 const commentArticleIDInput = document.getElementById('comment-article-id');
 const commentArea = document.getElementById('comment-area');
 const commentsContainer = document.getElementById('comments-container');
 const commentsCountElement = document.getElementById('comments-count');
+// If the form to add a comment exists (i.e: a user is logged in)
 if (addCommentForm) {
     addCommentForm.addEventListener('submit', (e) => {
         e.preventDefault();
+        // utilize access to post the data to the database and use the response to update the DOM without page refreshing
         axios
             .post('/article/comment', {
                 articleID: commentArticleIDInput.value,
                 comment: commentArea.value,
             })
             .then(function (response) {
-                let { comment, commentAuthor, commentsCount, commentDate } =
-                    response.data;
+                let { comment, commentAuthor, commentsCount, commentDate } = response.data;
                 const commentContainerElement = document.createElement('div');
                 const commentAuthorElement = document.createElement('div');
                 const commentDateElement = document.createElement('div');
@@ -33,8 +35,7 @@ if (addCommentForm) {
 
                 commentsCountElement.textContent = commentsCount;
 
-                commentContainerElement.className =
-                    'container single-comment-container mt-4 mb-4 mx-auto bg-gray-50 rounded-md p-4';
+                commentContainerElement.className = 'container single-comment-container mt-4 mb-4 mx-auto bg-gray-50 rounded-md p-4';
                 commentContainerElement.append(commentAuthorElement);
                 commentContainerElement.append(commentDateElement);
                 commentContainerElement.append(separator);
--- public/js/interactions.js
+++ public/js/interactions.js
@@ -1,14 +1,19 @@
+// grab the HTML elements using DOM Manipulation
 const elem = document.getElementById('likes-interact-form');
 const inputArticleID = document.getElementById('article-id-interact');
 const submitBtn = document.getElementById('submit-btn');
 let operation = '';
 let totalLikes = document.getElementById('total-likes');
+
+// Ensure the totalLikesCount is an integer to manipulate it when a new like is added/removed
 totalLikesCount = parseInt(totalLikes.textContent);
 
+// if the like button exists (i.e: a user is logged in)
 if (elem) {
     elem.addEventListener('submit', (e) => {
         e.preventDefault();
         operation = submitBtn.value;
+        // utilize access to post the data to the database and use the response to update the DOM without page refreshing
         axios
             .post('/article/interact', {
                 articleID: inputArticleID.value,
@@ -22,6 +27,7 @@ if (elem) {
                     submitBtn.value = 'Like';
                 }
                 totalLikesCount = response.data.numericLikeCount;
+                // If there is only 1 like, display the text as "1 Like", else: display the text as "<count> Likes"
                 totalLikes.textContent = totalLikesCount === 1 ? totalLikesCount + ' Like' : totalLikesCount + ' Likes';
             })
             .catch(function (error) {
