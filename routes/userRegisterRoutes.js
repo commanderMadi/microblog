bf703b9bf464eca02f0d0a5008cf1fd81b9731e2
Ahmed Magdy <madimagdy@gmail.com>
2023-07-22 18:53:27 +0900

    add comments to the userRegisterRoutes route

--- routes/userRegisterRoutes.js
+++ routes/userRegisterRoutes.js
@@ -1,41 +1,57 @@
 const express = require('express');
-const router = express.Router();
 const assert = require('assert');
 const bcrypt = require('bcrypt');
+
+// Custom middleware
 const checkUserAuth = require('../middleware/checkLoggedIn');
-const registerValidationSchema = require('../schemas/registerValidationSchema');
 const validate = require('../middleware/validate');
 
+// Custom validation schemas
+const registerValidationSchema = require('../schemas/registerValidationSchema');
+
+// Initialize the express router
+const router = express.Router();
+
+/**
+ * @desc Render the register page
+ * Passing in the custom middleware checkUserAuth to check whether the user is authenticated
+ * If the user is authenticated, they will be redirected to the home page
+ * If not, they can access the register page to create an account
+ */
 router.get('/', checkUserAuth, (req, res, next) => {
-    let query = `SELECT BlogSettings.user_id, Users.user_name, 
+    let getAllBlogSettingsQuery = `SELECT BlogSettings.user_id, Users.user_name, 
                   BlogSettings.blog_title, BlogSettings.blog_subtitle FROM BlogSettings
                   INNER JOIN Users ON BlogSettings.user_id=Users.user_id;`;
 
-    db.all(query, function (err, settingsRow) {
+    db.all(getAllBlogSettingsQuery, function (err, settingsRow) {
         if (err) {
             next(err);
         } else {
+            // Render the register page upon successful db operation
             res.render('register.ejs', { req, settingsRow });
         }
     });
 });
 
+/**
+ * @desc insert the registeration details and create a user
+ * Passing in the custom validation schema registerValidationSchema to ensure the user follows the specified registration criteria
+ */
 router.post('/', registerValidationSchema, validate, async (req, res, next) => {
     let { username, email, password } = req.body;
     let userRole = 'Reader';
-    let query = `SELECT * FROM Users WHERE user_email = ?`;
-
-    let secondQuery = `INSERT INTO Users ("user_name", "user_email", "user_password", "user_role")
+    let insertUserQuery = `INSERT INTO Users ("user_name", "user_email", "user_password", "user_role")
                  VALUES(?,?,?,?)
     `;
     // handle errors if any are sent via the validate middleware function
     let hashedPassword;
 
+    // hash the user password before sending it to the db
     if (typeof password !== 'undefined') {
         hashedPassword = await bcrypt.hash(password, 10);
     }
 
-    db.all(secondQuery, [username, email, hashedPassword, userRole], function (err, rows) {
+    db.all(insertUserQuery, [username, email, hashedPassword, userRole], function (err, rows) {
         if (err) {
             next(err);
         } else {
@@ -45,4 +61,5 @@ router.post('/', registerValidationSchema, validate, async (req, res, next) => {
     });
 });
 
+// export the router
 module.exports = router;
