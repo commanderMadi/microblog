723b1d6c1331b1bd544d9b7b315f10ceacc41116
Ahmed Magdy <madimagdy@gmail.com>
2023-07-22 18:42:16 +0900

    add comments to the readerRoutes route

--- routes/readerRoutes.js
+++ routes/readerRoutes.js
@@ -1,17 +1,22 @@
+// Require 3rd party dependencies
 const express = require('express');
-const router = express.Router();
 const assert = require('assert');
 const moment = require('moment');
-// const checkNotLoggedIn = require('../middleware/checkNotLoggedIn');
 
+// Initialize express router
+const router = express.Router();
+
+/**
+ * @desc Render the main homepage
+ */
 router.get('/', (req, res, next) => {
-    let query = 'SELECT * FROM Articles WHERE publish_state = "Published" ORDER BY publish_date DESC';
-    let secondQuery = `SELECT BlogSettings.user_id, Users.user_name, 
+    let getPublishedArticlesQuery = 'SELECT * FROM Articles WHERE publish_state = "Published" ORDER BY publish_date DESC';
+    let getAllBlogSettingsQuery = `SELECT BlogSettings.user_id, Users.user_name, 
                   BlogSettings.blog_title, BlogSettings.blog_subtitle FROM BlogSettings
                   INNER JOIN Users ON BlogSettings.user_id=Users.user_id;`;
 
-    db.all(query, function (errorArticles, rows) {
-        db.all(secondQuery, function (errBlog, settingsRow) {
+    db.all(getPublishedArticlesQuery, function (errorArticles, rows) {
+        db.all(getAllBlogSettingsQuery, function (errBlog, settingsRow) {
             let authorFirstTimeLogin;
             if (errorArticles) {
                 next(errorArticles);
@@ -19,10 +24,12 @@ router.get('/', (req, res, next) => {
             if (errBlog) {
                 next(errBlog);
             } else {
+                // Display a change password notification if the author logs in for the first time
                 if (req.user && req.user.user_role === 'Author') {
                     let { user_password } = req.user;
                     authorFirstTimeLogin = user_password === process.env.FIRST_TIME_LOGIN_PASSWORD || user_password === 'admintest123';
                 }
+                // Render the homepage on successful db operation
                 res.render('index.ejs', {
                     authorFirstTimeLogin,
                     rows,
@@ -34,18 +41,21 @@ router.get('/', (req, res, next) => {
     });
 });
 
+/**
+ * @desc Render the individual article page, based on the article ID supplies as a request parameter
+ */
 router.get('/article/:id', (req, res, next) => {
-    let query = 'SELECT * FROM Articles WHERE article_id = ? ;';
-    let secondQuery = 'SELECT * FROM Comments WHERE article_id = ?';
-    let thirdQuery = 'SELECT * FROM Likes WHERE article_id = ?';
-    let fourthQuery = `SELECT BlogSettings.user_id, Users.user_name, 
+    let getArticleQuery = 'SELECT * FROM Articles WHERE article_id = ? ;';
+    let getArticleCommentsQuery = 'SELECT * FROM Comments WHERE article_id = ?';
+    let getArticleLikesQuery = 'SELECT * FROM Likes WHERE article_id = ?';
+    let getAllBlogSettingsQuery = `SELECT BlogSettings.user_id, Users.user_name, 
                   BlogSettings.blog_title, BlogSettings.blog_subtitle FROM BlogSettings
                   INNER JOIN Users ON BlogSettings.user_id=Users.user_id;`;
     let articleID = req.params.id;
-    db.all(query, [articleID], function (err, rows) {
-        db.all(secondQuery, [articleID], function (commentsErr, commentsRows) {
-            db.all(thirdQuery, [articleID], function (likesError, likesRows) {
-                db.all(fourthQuery, function (settingsError, settingsRow) {
+    db.all(getArticleQuery, [articleID], function (err, rows) {
+        db.all(getArticleCommentsQuery, [articleID], function (commentsErr, commentsRows) {
+            db.all(getArticleLikesQuery, [articleID], function (likesError, likesRows) {
+                db.all(getAllBlogSettingsQuery, function (settingsError, settingsRow) {
                     if (err) {
                         next(err);
                     }
@@ -58,6 +68,7 @@ router.get('/article/:id', (req, res, next) => {
                     if (settingsError) {
                         next(settingsError);
                     } else {
+                        // Render the article view upon successful db operation
                         res.render('article.ejs', {
                             req,
                             rows,
@@ -73,6 +84,9 @@ router.get('/article/:id', (req, res, next) => {
     });
 });
 
+/**
+ * @desc Add a comment to a specific article
+ */
 router.post('/article/comment', (req, res, next) => {
     let insertQuery = `INSERT INTO Comments ("article_id", "comment_text", "comment_author", "comment_date", "user_id")
                 VALUES (?,?,?,?, ?)`;
@@ -91,12 +105,16 @@ router.post('/article/comment', (req, res, next) => {
         if (err) {
             next(err);
         } else {
+            // commentsCount will be used to load the number of comments on the frontend using axios (ajax request)
             commentsCount = rows.length;
+            // send the data to the client. Axios will use these data to ajax load the comment details upon posting it without a page refresh
             res.send({ comment, commentAuthor, commentsCount, commentDate: date });
         }
     });
 });
-
+/**
+ * @desc Add a like to a specific article
+ */
 router.post('/article/interact', (req, res, next) => {
     let updateQuery = `UPDATE Articles SET "likes_count" = ? WHERE article_id = ?`;
     let insertQuery = `INSERT INTO Likes ("user_id", "article_id")
@@ -110,18 +128,21 @@ router.post('/article/interact', (req, res, next) => {
     let likesCountAfterInteraction = numericLikeCount;
 
     switch (operation) {
+        // If the user pressed like, likes count will increase
         case 'Like':
             likesCountAfterInteraction++;
             db.all(insertQuery, [userID, articleID], function (err, rows) {
                 if (err) {
                     next(err);
                 } else {
+                    // send the data to the client. Axios will use these data to ajax load the total likes count upon posting it without a page refresh
                     res.send({
                         numericLikeCount: likesCountAfterInteraction,
                     });
                 }
             });
             break;
+        // If the user pressed unlike, likes count will decrease
         case 'Unlike':
             likesCountAfterInteraction--;
             db.all(deleteQuery, [userID], function (err, rows) {
@@ -129,6 +150,7 @@ router.post('/article/interact', (req, res, next) => {
                     next(err);
                 } else {
                     res.send({
+                        // send the data to the client. Axios will use these data to ajax load the total likes count upon posting it without a page refresh
                         numericLikeCount: likesCountAfterInteraction,
                     });
                 }
@@ -141,7 +163,9 @@ router.post('/article/interact', (req, res, next) => {
         }
     });
 });
-
+/**
+ * @desc send back the user to the homepage upon logging out
+ */
 router.get('/logout', (req, res, next) => {
     req.logout((err) => {
         if (err) {
