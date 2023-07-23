9c97835eb55c75b7a20e8b339a64c57423fde0a0
Ahmed Magdy <madimagdy@gmail.com>
2023-07-23 14:01:53 +0900

    update data input requirements

--- schemas/updateValidationSchema.js
+++ schemas/updateValidationSchema.js
@@ -5,9 +5,15 @@ const { body } = require('express-validator');
 // all fields must not be empty
 // fields have a min and max length except for the article body (contents)
 const updateSettingsValidationSchema = [
-    body('title', 'Article title minimum length is 5 and maximum is 50.').exists().isLength({ min: 5, max: 50 }),
-    body('subtitle', 'Article title minimum length is 5 and maximum is 50.').exists().isLength({ min: 5, max: 50 }),
-    body('contents', 'Article content minimum length is 3').exists().isLength({ min: 3, max: undefined }),
+    body('title', 'Article title minimum length is 5 and maximum is 50.')
+        .exists()
+        .isLength({ min: 5, max: 50 }),
+    body('subtitle', 'Article title minimum length is 5 and maximum is 50.')
+        .exists()
+        .isLength({ min: 5, max: 50 }),
+    body('contents', 'Article content minimum length is 3')
+        .exists()
+        .isLength({ min: 3, max: undefined }),
 ];
 
 // export the schema
